9614e48715849588244e20d14cdf658441cc4318
Pavel Andreikovets <potto8989@mail.ru>
2023-01-17 11:28:17 +0300

    feat: add hook useState and bootsrap

---
+++ my-app/src/components/useStateExample/Component.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { Button, Card } from "react-bootstrap";
+
+const getInitialState = () => false;
+
+export function Component() {
+  const [visible, setVisibility] = useState(getInitialState);
+  const handleClick = () => {
+    setVisibility((currentValue) => !currentValue);
+  };
+  return (
+    <div className="container">
+      <h1>What is React</h1>
+
+      <Button variant="primary" onClick={handleClick}>
+        {visible ? "Hide" : "Show"}
+      </Button>
+      {visible && (
+        <Card>
+          <Card.Body>
+            A JavaScript library for building user interfaces
+          </Card.Body>
+        </Card>
+      )}
+    </div>
+  );
+}
