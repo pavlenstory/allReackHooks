819f0e9804a6f21640bf29ecc178034a3373e361
Pavel Andreikovets <potto8989@mail.ru>
2023-01-20 20:36:32 +0300

    feat: add hook useLayoutEffect

--- my-app/src/components/useLayoutEffectExample/ComponentUseLayoutEffect.js
+++ my-app/src/components/useLayoutEffectExample/ComponentUseLayoutEffect.js
@@ -1,3 +1,19 @@
+import { useEffect, useLayoutEffect } from "react";
+
+const longRunningTask = () => {
+  for (let i = 0; i < 99999; i++) {
+    console.log();
+  }
+};
+
 export function ComponentUseLayoutEffect() {
+  console.log("start");
+  useEffect(() => console.log("useEffect"), []);
+  useLayoutEffect(() => {
+    console.log("useLayoutEffect start");
+    longRunningTask();
+    console.log("useLayoutEffect end");
+  }, []);
+
   return <div className="container"></div>;
 }
